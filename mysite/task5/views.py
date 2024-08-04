e3b3ddf38f7932841d8ec1cf43d00503e4a7df59
tsars <tsarskiytsarskiy@mail.ru>
2024-08-04 21:58:46 +0700

    5553

--- mysite/task5/views.py
+++ mysite/task5/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 from django.shortcuts import render
-
+info = {'error': 'Вы должны быть старше 18'}
 def index(request):
     if request.method == 'Post':
         login = request.Post.get('login')
@@ -16,6 +16,10 @@ def index(request):
         print(password2)
         print(age)
 
+        if password1 == password2 and int(age) > 18:
+            return HttpResponse(f"Приветствуем, {login}")
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18'
+            return render(request, 'fifth_task/registration_page.html')
 
-        return HttpResponse(f"Приветствуем, {login}")
-    return render(request, 'fifth_task/registration_page.html')
+    return render(request, 'fifth_task/registration_page.html', context=info)
